39cab98dfdc50f71bacb7bbdf6cb76d0a92e151d
HACKER21078 <94623329+HACKER21078@users.noreply.github.com>
2025-06-18 22:53:54 +0200

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const fetch = require('node-fetch');
 const app = express();
+app.use = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/stream', async (req, res) => {
